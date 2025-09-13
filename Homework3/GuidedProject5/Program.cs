227dea714e1b9515312d8f42889acc6cc1bf2605
vlg425 <114525880+vlg425@users.noreply.github.com>
2025-09-12 20:54:57 -0400

    created assign group method, used loop to divide animals into groups

--- Homework3/GuidedProject5/Program.cs
+++ Homework3/GuidedProject5/Program.cs
@@ -8,15 +8,15 @@ string[] pettingZoo =
 };
 
 RandomizeAnimals();
-// string[,] group = AssignGroup();
+string[,] group = AssignGroup();
 Console.WriteLine("School A");
 // PrintGroup(group);
 
-void RandomizeAnimals()
+void RandomizeAnimals() 
 {
     Random random = new Random();
 
-    for (int i = 0; i < pettingZoo.Length; i++)
+    for (int i = 0; i < pettingZoo.Length; i++) 
     {
         int r = random.Next(i, pettingZoo.Length);
 
@@ -25,7 +25,20 @@ void RandomizeAnimals()
         pettingZoo[i] = temp;
     }
 }
-// foreach(string animal in pettingZoo) 
-// {
-//     Console.WriteLine(animal);
-// }
+
+string[,] AssignGroup(int groups = 6) 
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
+}
+
