7a9d34f69ad077d8e9c7b2fd24fe8fe80fb95008
vlg425 <114525880+vlg425@users.noreply.github.com>
2025-09-12 21:03:20 -0400

    previous comment meant for this commit

--- Homework3/GuidedProject5/Program.cs
+++ Homework3/GuidedProject5/Program.cs
@@ -7,10 +7,17 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-RandomizeAnimals();
-string[,] group = AssignGroup();
-Console.WriteLine("School A");
-// PrintGroup(group);
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
+
+void PlanSchoolVisit(string schoolName, int groups = 6) 
+{
+    RandomizeAnimals(); 
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
+}
 
 void RandomizeAnimals() 
 {
@@ -42,3 +49,15 @@ string[,] AssignGroup(int groups = 6)
     return result;
 }
 
+void PrintGroup(string[,] groups) 
+{
+    for (int i = 0; i < groups.GetLength(0); i++) 
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < groups.GetLength(1); j++) 
+        {
+            Console.Write($"{groups[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
